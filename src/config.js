b78bfc586ca09ba47e0cd16dcbaaa9da27c4839a
Michael Weedman <michael.weedman@gmail.com>
2020-01-10 07:46:00 -0700

    config edits

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ADDRESS: process.env.API_ADDRESS ||  'https://infinite-coast-46458.herokuapp.com/api',
-  TOKEN_KEY: process.env.TOKEN_KEY
+  API_ADDRESS: 'https://infinite-coast-46458.herokuapp.com/api',
+  TOKEN_KEY: 'My_Token'
 }
